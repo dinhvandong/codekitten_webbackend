bf5199664ed755a873350588bd3abbf17b2909e0
dinhvandong <dongvan.ict3@gmail.com>
2021-05-03 13:16:04 +0700

    changdb

--- config_server.js
+++ config_server.js
@@ -1,5 +1,5 @@
 
-const ConfigServer = {host:"http://localhost:8080"};
+const ConfigServer = {host:"http://128.199.145.159:8080"};
 //http://dev.codekitten.asia
 //export default ConfigServer;
 module.exports = { ConfigServer };
